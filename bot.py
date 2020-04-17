6891b417008ff7f21d961cd7274e68f25a6454e1
Eduard Varshavsky <47066749+EduardVar@users.noreply.github.com>
2020-04-16 22:12:19 -0400

    Adds ability to use current hour and see
    Converts from integer to the name of the channel. Sees everyone in current channel

--- bot.py
+++ bot.py
@@ -2,13 +2,21 @@
 import os
 import random
 
+import datetime
+
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+
 roleID = "<@&675471556926767164>"
+currHour = datetime.datetime.now().hour
+hourDic = {0:"12 am", 1:"1 am", 2:"2 am", 3:"3 am", 4:"4 am", 5:"5 am",
+           6:"6am", 7:"7 am", 8:"8 am", 9:"9 am", 10:"10 am", 11:"11 am",
+           12:"12 pm", 13:"1 pm", 14:"2 pm", 15:"3 pm", 16:"4 pm", 17:"5 pm",
+           18:"6 pm", 19:"7 pm", 20:"8 pm", 21:"9 pm", 22:"10 pm", 23:"11 pm"}
 
 client = discord.Client()
 
@@ -22,6 +30,8 @@ async def on_ready():
 
     #for r in guild.roles:
         #print(r)
+
+    print(hourDic[currHour])
 
 @client.event
 async def on_member_join(member):
@@ -53,7 +63,8 @@ async def on_message(message):
         raise discord.DiscordException
 
     elif mContent == '~users':
-        voiceChannel = discord.utils.get(guild.voice_channels, name="8 pm")
+        voiceChannel = discord.utils.get(guild.voice_channels,
+                                         name=hourDic[currHour])
 
         for member in voiceChannel.members:
             await message.channel.send(member);
