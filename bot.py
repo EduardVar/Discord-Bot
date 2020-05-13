09bf917c5d1a3253be0af03cf18ea4a6c0b4c7ed
Eduard Varshavsky <47066749+EduardVar@users.noreply.github.com>
2020-05-13 19:46:33 -0400

    Now uses bot command.Bot rather than client

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import asyncio
 
 from main_uses import *
 
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,33 +15,33 @@ ID = os.getenv('CLIENT_ID')
 SECRET = os.getenv('CLIENT_SECRET')
 AGENT = os.getenv('USER_AGENT')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='~')
 guild = discord.guild.Guild
 initPraw(ID, SECRET, AGENT)
 
 async def move_task():
-    await client.wait_until_ready()
+    await bot.wait_until_ready()
 
     global guild
-    guild = discord.utils.get(client.guilds, name=GUILD)   
+    guild = discord.utils.get(bot.guilds, name=GUILD)
     streamHour, userStreaming, wasStreaming = await setupMoveTask(guild)
     
-    while not client.is_closed():
+    while not bot.is_closed():
         uS, wS = await timeChangeLogic(guild, streamHour,
                                        userStreaming, wasStreaming)
         userStreaming, wasStreaming = uS, wS
 
         await asyncio.sleep(1) # task runs every second
 
-@client.event
+@bot.event
 async def on_ready():
     global guild
     print(
-        f'{client.user.name} is connected to the following guild:\n'
+        f'{bot.user.name} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
-@client.event
+@bot.event
 async def on_member_join(member):
     global guild
     await member.create_dm()
@@ -48,12 +49,18 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to {guild.name}!'
     )
 
-@client.event
-async def on_message(message): 
-    if message.author == client.user:
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
         return
 
     await checkMessage(guild, message, message.content)
 
-client.loop.create_task(move_task())
-client.run(TOKEN)
+# Won't run while on_message is a thing (will have to test)
+@bot.command(name='tester', help='Trying to see if help works with test too!')
+async def testerCommand(ctx):
+    response = "It's working"
+    await ctx.send(response)
+
+bot.loop.create_task(move_task())
+bot.run(TOKEN)
